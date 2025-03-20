b5e0741c3a471a0c0a21d4e564baf2a024b7bf58
lucas <lucasclark997@gmail.com>
2025-03-20 15:20:49 -0300

    Se agregan comentarios en el archivo main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ led_board = Pin("LED", Pin.OUT)
 sleep(1)    #le damos tiempo a vREPL
 print("\nLED esta destellando...")
 while True:
+    #se puede utilizar sin try-except pero muestra el mensaje de que no se manejo la excepcion
     try:
-        led_board.toggle()
-        # led_board.value(not led_board.value())
-        sleep(.5) # sleep 1sec
+        led_board.toggle() #solo para raspberry pi pico
+        # led_board.value(not led_board.value()) # para esp32, lee el estado y lo invierte
+        sleep(1) # sleep 1sec
     except KeyboardInterrupt:
         break
 led_board.off()
